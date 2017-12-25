734adeb794e582ecbf89da754d423f81e6c6a863
Spencer Turkel <spencer@palmbeachsoftware.com>
2017-12-25 15:19:59 -0500

    Writing Day3PartTwo

--- src/StreamRecursion.hs
+++ src/StreamRecursion.hs
@@ -1,11 +1,14 @@
 module StreamRecursion where
 
-import Data.Stream ((<:>), Stream)
+import Data.Functor.Foldable (Base, Corecursive(embed), Recursive(project))
+import Data.Stream (Stream(Cons))
 
-data StreamF a f = Cons a f
-
-type instance Base (Stream a) = StreamF a
+type instance Base (Stream a) = (,) a
 
 instance Corecursive (Stream a) where
-  embed :: StreamF a (Stream a) -> Stream a
-  embed (Cons x xs) = x <:> xs
+  embed :: (a, Stream a) -> Stream a
+  embed (x, xs) = Cons x xs
+
+instance Recursive (Stream a) where
+  project :: Stream a -> (a, Stream a)
+  project (Cons x xs) = (x, xs)
