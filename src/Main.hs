e92cd6512dc6e3aae6e35c9cf0b0707ab2c62ed9
Spencer Turkel <spencer@palmbeachsoftware.com>
2017-12-25 11:54:09 -0500

    Solve Day3 part one

--- src/Main.hs
+++ src/Main.hs
@@ -4,5 +4,26 @@ import qualified Data.Stream as S
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
-  print . S.take 10 $ S.repeat (2 :: Int)
+  putStrLn "Calculating..."
+  let point = (spiral :: S.Stream (Point Int)) S.!! (289326 - 1)
+  putStrLn $ "The point is " ++ show point
+  let distanceFromOrigin = abs (x point) + abs (y point)
+  putStrLn $ "The distance from the origin is " ++ show distanceFromOrigin
+
+data Point a = Point { x :: a, y :: a } deriving (Show)
+
+spiral :: (Ord a, Num a) => S.Stream (Point a)
+spiral = S.iterate nextSpiralStep (Point 0 0)
+
+nextSpiralStep :: (Ord a, Num a) => Point a -> Point a
+nextSpiralStep (Point{x, y})
+  | x == 0 && y == 0 = Point {x = 1, y = 0}
+  | x > 0 && x == y = Point {x = x - 1, y}
+  | x < 0 && negate x == y = Point {x, y = y - 1}
+  | x < 0 && x == y = Point {x = x + 1, y}
+  | x > 0 && negate x == y = Point {x = x + 1, y}
+  | x > 0 && y < x && y > negate x = Point {x, y = y + 1}
+  | x < 0 && y > x && y < negate x = Point {x, y = y - 1}
+  | y > 0 && x < y && x > negate y = Point {x = x - 1, y}
+  | y < 0 && x > y && x < negate y = Point {x = x + 1, y}
+  | otherwise = undefined
