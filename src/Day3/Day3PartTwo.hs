f186c8d4e9a875f0fb5872c2e4b1170c52a0aab0
Spencer Turkel <spencer@palmbeachsoftware.com>
2017-12-26 01:24:09 -0500

    Typechecking PartTwo

--- src/Day3/Day3PartTwo.hs
+++ src/Day3/Day3PartTwo.hs
@@ -1,9 +1,11 @@
 module Day3PartTwo where
 
-import Data.Foldable (find)
-import qualified Data.Map.Lazy as Map (Map, fromList, lookup)
-import Data.Monoid (Sum(getSum))
-import Data.Stream (Stream, filter, fromList, head, repeat, toList, zip)
+import Data.List (zip)
+import Data.Map.Lazy (Map, (!), insert)
+import qualified Data.Map.Lazy as Map (fromList)
+import Data.Monoid (Sum(Sum), (<>))
+import Data.Stream (Stream)
+import qualified Data.Stream as Stream (toList)
 import Prelude hiding (Left, Right, (!!), filter, head, repeat, zip)
 
 import Point (Point(Point, x, y))
@@ -12,49 +14,37 @@ import qualified Spiral (spiral)
 runDayThreePartTwo :: IO ()
 runDayThreePartTwo = do
   putStrLn "Calculating..."
-  let spiral = spiralSum (Spiral.spiral :: Stream (Point Int))
-      result = findS (\x -> x > 289326) . fmap getSum $ spiral
+  let Sum result =
+        spiralSum (Spiral.spiral :: Stream (Point Int)) (Sum 289326 :: Sum Int)
       message = "The result is " ++ show result
   message `seq` putStrLn message
 
 spiralSum ::
-     forall a m. (Eq a, Num a, Monoid m)
+     forall a m. (Ord a, Num a, Monoid m, Ord m)
   => Stream (Point a)
-  -> Stream m
-spiralSum =
-  let updatePoint :: Point a -> (Point a -> m) -> (Point a -> m -> b) -> b
-      updatePoint =
-        let getNext :: (Point a -> m) -> Point a -> m
-            getNext f = sumAtPoint (fmap f . offsetPoint)
-        in updatePointSum getNext
-      go :: Point a -> Stream (Point a, m) -> Stream (Point a, m)
-      go p xs =
-        let ps = Map.fromList $ toList xs
-        in _
-  in fmap snd . go (Point 0 0) . flip zip (repeat (mempty :: m))
+  -> m
+  -> m
+spiralSum (Stream.toList -> spiral) =
+  let pointValues = Map.fromList . fmap (flip (,) mempty) $ spiral
+      nextPoint = (!) . Map.fromList $ zip spiral (tail spiral)
+  in go nextPoint pointValues (Point 0 0)
   where
-    updatePointSum ::
-         ((Point a -> m) -> Point a -> m)
-      -> Point a
-      -> (Point a -> m)
-      -> (Point a -> m -> b)
-      -> b
-    updatePointSum findNewPointSumGivenExistingSums p findPoint updatePointMap =
-      updatePointMap p . findNewPointSumGivenExistingSums findPoint $ p
-    sumAtPoint :: (Direction -> Point a -> m) -> Point a -> m
-    sumAtPoint adjacentSum =
-      mconcat . fmap adjacentSum $ [Left, Right, Up, Down]
-    offsetPoint :: Direction -> Point a -> Point a
-    offsetPoint Left Point {x, y} = Point {x = x - 1, y}
-    offsetPoint Right Point {x, y} = Point {x = x + 1, y}
-    offsetPoint Up Point {x, y} = Point {x, y = y + 1}
-    offsetPoint Down Point {x, y} = Point {x, y = y - 1}
-
-findS :: (a -> Bool) -> Stream a -> a
-findS p = head . filter p
-
-data Direction
-  = Left
-  | Right
-  | Up
-  | Down
+    go :: (Point a -> Point a) -> Map (Point a) m -> Point a -> m -> m
+    go nextPoint pointValues currentPoint valueToFind
+      | valueToFind >= pointValues ! currentPoint = valueToFind
+      | otherwise =
+        go
+          nextPoint
+          (updatePointValues currentPoint pointValues)
+          (nextPoint currentPoint)
+          valueToFind
+      where
+        updatePointValues :: Point a -> Map (Point a) m -> Map (Point a) m
+        updatePointValues p ps = insert p (calculatePointValue ps p) ps
+        calculatePointValue :: Map (Point a) m -> Point a -> m
+        calculatePointValue ps (Point {x, y}) =
+          let leftValue = ps ! (Point {x = x - 1, y})
+              rightValue = ps ! Point {x = x + 1, y}
+              aboveValue = ps ! Point {x, y = y + 1}
+              belowValue = ps ! Point {x, y = y - 1}
+          in leftValue <> rightValue <> aboveValue <> belowValue
