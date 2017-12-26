ed94da582a75d9ec5f58d83e89d755827ab7748a
Spencer Turkel <spencer@palmbeachsoftware.com>
2017-12-25 22:51:24 -0500

    Writing PartTwo

--- src/Day3/Day3PartTwo.hs
+++ src/Day3/Day3PartTwo.hs
@@ -1,12 +1,12 @@
 module Day3PartTwo where
 
 import Control.Comonad.Trans.Env (EnvT)
-import Data.Functor.Foldable (gpara, para)
+import Data.Functor.Foldable (ana)
 import Data.List (elemIndex)
 import Data.Maybe (fromMaybe)
 import Data.Monoid (Sum(Sum, getSum))
-import Data.Stream (Stream, filter, head, zip)
-import Prelude hiding ((!!), filter, head, Left, Right, zip)
+import Data.Stream (Stream, filter, head, repeat, zip)
+import Prelude hiding (Left, Right, (!!), filter, head, repeat, zip)
 
 import Point (Point(Point, x, y))
 import qualified Spiral (spiral)
@@ -24,17 +24,22 @@ spiralSum ::
      forall a m. (Eq a, Num a, Monoid m)
   => Stream (Point a)
   -> Stream m
-spiralSum spiral = para (f . fmap (uncurry zip)) spiral
+spiralSum =
+  let getNext :: Stream (Point a, m) -> Point a -> m
+      getNext xs = sumAtPoint (fmap (findPointValue xs) . offsetPoint)
+
+  in fmap snd . go getNext (Point 0 0) . flip zip (repeat (mempty :: m))
   where
-    f :: (Point a, Stream (Point a, m)) -> Stream m
-    f = _
+    go :: (Stream (Point a, m) -> Point a -> m) -> Point a -> Stream (Point a, m) -> Stream (Point a, m)
+    go next p xs = _
     sumAtPoint :: (Direction -> Point a -> m) -> Point a -> m
-    sumAtPoint adjacentSum = mconcat . fmap adjacentSum $ [Left, Right, Up, Down]
+    sumAtPoint adjacentSum =
+      mconcat . fmap adjacentSum $ [Left, Right, Up, Down]
     offsetPoint :: Direction -> Point a -> Point a
-    offsetPoint Left Point{x, y} = Point{x = x - 1, y}
-    offsetPoint Right Point{x, y} = Point{x = x + 1, y}
-    offsetPoint Up Point{x, y} = Point{x, y = y + 1}
-    offsetPoint Down Point{x, y} = Point{x, y = y - 1}
+    offsetPoint Left Point {x, y} = Point {x = x - 1, y}
+    offsetPoint Right Point {x, y} = Point {x = x + 1, y}
+    offsetPoint Up Point {x, y} = Point {x, y = y + 1}
+    offsetPoint Down Point {x, y} = Point {x, y = y - 1}
     findPointValue :: Stream (Point a, b) -> Point a -> b
     findPointValue ps p = snd $ find (\(q, _) -> q == p) ps
 
