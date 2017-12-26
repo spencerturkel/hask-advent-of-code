7431fbfbaa39fded79f3a8dbc3148eeef1ef9cb7
Spencer Turkel <spencer@palmbeachsoftware.com>
2017-12-26 15:54:10 -0500

    Solve Day4PartOne

--- src/Day4/Day4PartOne.hs
+++ src/Day4/Day4PartOne.hs
@@ -1,11 +1,10 @@
 module Day4PartOne where
 
+import Data.List ((\\), nub)
+
 import Paths_HaskAdventOfCode (getDataFileName)
 
 runDay4PartOne :: IO ()
 runDay4PartOne = do
-    fileName <- getDataFileName "input/day4.txt"
-    putStrLn fileName
-    input <- readFile fileName
-    print $ take 2 $ lines input
-    return ()
+    input <- getDataFileName "input/day4.txt" >>= readFile
+    print . length . filter (null) . fmap (unwords . (\xs -> xs \\ nub xs) . words) . lines $ input
