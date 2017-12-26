539b1a8e169d31ad376bc5f0a80e3a4ba06bdff9
Spencer Turkel <spencer@palmbeachsoftware.com>
2017-12-26 14:10:33 -0500

    Reimplemented without HashMap

--- src/Day3/Day3PartTwo.hs
+++ src/Day3/Day3PartTwo.hs
@@ -1,5 +1,6 @@
 module Day3PartTwo where
 
+import Data.Functor.Identity (Identity(Identity, runIdentity))
 import Data.List (lookup, zip)
 import Data.Maybe (fromJust)
 import Data.Monoid (Sum(Sum), (<>))
@@ -14,23 +15,15 @@ runDayThreePartTwo :: IO ()
 runDayThreePartTwo = do
   putStrLn "Calculating..."
   let Sum result =
-        spiralSum (Spiral.spiral :: Stream (Point Int)) (Sum 289326 :: Sum Int)
+        spiralSum (Spiral.spiral :: Stream (Point Int)) (Sum (289326 - 1 :: Int))
       message = "The result is " ++ show result
   message `seq` putStrLn message
 
-data Mapping a b = Mapping
-  { at :: a -> b
-  , insert :: a -> b -> Mapping a b
-  }
+mappingFromList :: (Eq a) => [(a, b)] -> a -> Maybe b
+mappingFromList = flip lookup
 
-mappingFromList :: (Eq a) => [(a, b)] -> Mapping a (Maybe b)
-mappingFromList xs =
-  let at = endoMapFromList xs
-      insert x y = mappingFromList ((x, y) : xs)
-  in Mapping {at, insert}
-
-endoMapFromList :: (Eq a) => [(a, b)] -> a -> Maybe b
-endoMapFromList = flip lookup
+updateMapping :: (Applicative f, Eq a) => a -> b -> (a -> f b) -> a -> f b
+updateMapping x y f a = if a == x then pure y else f a
 
 spiralSum ::
      forall a m. (Ord a, Num a, Monoid m, Ord m)
@@ -40,26 +33,23 @@ spiralSum ::
 spiralSum (Stream.toList -> spiral) =
   let pointValues =
         fmap fromJust . mappingFromList . fmap (flip (,) mempty) $ spiral
-      nextPoint = fmap fromJust . endoMapFromList $ zip spiral (tail spiral)
+      nextPoint = fmap fromJust . mappingFromList $ zip spiral (tail spiral)
   in go nextPoint pointValues (Point 0 0)
   where
-    go :: (Point a -> Point a) -> Mapping (Point a) m -> Point a -> m -> m
+    go :: (Point a -> Point a) -> (Point a -> m) -> Point a -> m -> m
     go nextPoint pointValues currentPoint valueToFind
-      | valueToFind >= pointValues `at` currentPoint = valueToFind
+      | valueToFind > pointValues currentPoint = valueToFind
       | otherwise =
         go
           nextPoint
-          (updatePointValues currentPoint pointValues)
+          (runIdentity . updateMapping currentPoint (calculatePointValue pointValues currentPoint) (Identity .pointValues))
           (nextPoint currentPoint)
           valueToFind
       where
-        updatePointValues ::
-             Point a -> Mapping (Point a) m -> Mapping (Point a) m
-        updatePointValues p ps = insert ps p (calculatePointValue ps p)
-        calculatePointValue :: Mapping (Point a) m -> Point a -> m
+        calculatePointValue :: (Point a -> m) -> Point a -> m
         calculatePointValue ps (Point {x, y}) =
-          let leftValue = ps `at` (Point {x = x - 1, y})
-              rightValue = ps `at` Point {x = x + 1, y}
-              aboveValue = ps `at` Point {x, y = y + 1}
-              belowValue = ps `at` Point {x, y = y - 1}
+          let leftValue = ps $ Point {x = x - 1, y}
+              rightValue = ps $ Point {x = x + 1, y}
+              aboveValue = ps $ Point {x, y = y + 1}
+              belowValue = ps $ Point {x, y = y - 1}
           in leftValue <> rightValue <> aboveValue <> belowValue
