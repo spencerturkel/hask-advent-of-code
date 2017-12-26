3a72f4b4bd25e4520da023e4a03b63c54e80e87c
Spencer Turkel <spencer@palmbeachsoftware.com>
2017-12-26 14:53:29 -0500

    Solved Day3PartTwo

--- src/Day3/Day3PartTwo.hs
+++ src/Day3/Day3PartTwo.hs
@@ -3,7 +3,7 @@ module Day3PartTwo where
 import Data.Functor.Identity (Identity(Identity, runIdentity))
 import Data.List (lookup, zip)
 import Data.Maybe (fromJust)
-import Data.Monoid (Sum(Sum), (<>))
+import Data.Monoid (Sum(Sum))
 import Data.Stream (Stream)
 import qualified Data.Stream as Stream (toList)
 import Prelude hiding (Left, Right, (!!), filter, head, repeat, zip)
@@ -15,41 +15,57 @@ runDayThreePartTwo :: IO ()
 runDayThreePartTwo = do
   putStrLn "Calculating..."
   let Sum result =
-        spiralSum (Spiral.spiral :: Stream (Point Int)) (Sum (289326 - 1 :: Int))
+        spiralSum
+          (Spiral.spiral :: Stream (Point Int))
+          (Sum (289326 - 1 :: Int))
       message = "The result is " ++ show result
   message `seq` putStrLn message
 
 mappingFromList :: (Eq a) => [(a, b)] -> a -> Maybe b
 mappingFromList = flip lookup
 
-updateMapping :: (Applicative f, Eq a) => a -> b -> (a -> f b) -> a -> f b
+updateMapping ::
+     (Applicative f, Eq a) => a -> b -> (a -> f b) -> a -> f b
 updateMapping x y f a = if a == x then pure y else f a
 
 spiralSum ::
-     forall a m. (Ord a, Num a, Monoid m, Ord m)
+     forall a m. (Ord a, Num a, Monoid m, Num m, Ord m)
   => Stream (Point a)
   -> m
   -> m
 spiralSum (Stream.toList -> spiral) =
   let pointValues =
-        fmap fromJust . mappingFromList . fmap (flip (,) mempty) $ spiral
+        fmap fromJust .
+        updateMapping (Point 0 0) 1 . mappingFromList . fmap (flip (,) mempty) $
+        spiral
       nextPoint = fmap fromJust . mappingFromList $ zip spiral (tail spiral)
-  in go nextPoint pointValues (Point 0 0)
+  in go nextPoint pointValues (Point {x = 1, y = 0}) 1
   where
-    go :: (Point a -> Point a) -> (Point a -> m) -> Point a -> m -> m
-    go nextPoint pointValues currentPoint valueToFind
-      | valueToFind > pointValues currentPoint = valueToFind
+    go :: (Point a -> Point a) -> (Point a -> m) -> Point a -> m -> m -> m
+    go nextPoint pointValues currentPoint previousValue valueToFind
+      | previousValue > valueToFind = previousValue
       | otherwise =
-        go
-          nextPoint
-          (runIdentity . updateMapping currentPoint (calculatePointValue pointValues currentPoint) (Identity .pointValues))
-          (nextPoint currentPoint)
-          valueToFind
+        let newValue = calculatePointValue pointValues currentPoint
+        in go
+             nextPoint
+             (runIdentity .
+              updateMapping currentPoint newValue (Identity . pointValues))
+             (nextPoint currentPoint)
+             newValue
+             valueToFind
       where
         calculatePointValue :: (Point a -> m) -> Point a -> m
         calculatePointValue ps (Point {x, y}) =
-          let leftValue = ps $ Point {x = x - 1, y}
-              rightValue = ps $ Point {x = x + 1, y}
-              aboveValue = ps $ Point {x, y = y + 1}
-              belowValue = ps $ Point {x, y = y - 1}
-          in leftValue <> rightValue <> aboveValue <> belowValue
+          foldMap
+            getValue
+            [ (x - 1, y)
+            , (x - 1, y + 1)
+            , (x, y + 1)
+            , (x + 1, y + 1)
+            , (x + 1, y)
+            , (x + 1, y - 1)
+            , (x, y - 1)
+            , (x - 1, y - 1)
+            ]
+          where
+            getValue (x', y') = ps $ Point {x = x', y = y'}
