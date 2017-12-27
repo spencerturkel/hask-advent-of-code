7ddbc1dc06a35cbf2e0cfd27a6956b58cc501f76
Spencer Turkel <spencer@palmbeachsoftware.com>
2017-12-27 12:00:23 -0500

    Refactoring Day5

--- src/Day5.hs
+++ src/Day5.hs
@@ -9,28 +9,36 @@ import Debug.Trace (trace)
 import Paths_HaskAdventOfCode (getDataFileName)
 
 runDay5PartOne :: IO ()
-runDay5PartOne = runDay5 . OffsetAdjuster $ fmap (+ 1)
+runDay5PartOne = runDay5WithAdjuster adjustPartOne
 
 runDay5PartTwo :: IO ()
-runDay5PartTwo =
-  runDay5 . OffsetAdjuster . fmap $ \x ->
+runDay5PartTwo = runDay5WithAdjuster adjustPartTwo
+
+adjustPartOne :: Int -> Int
+adjustPartOne = (+ 1)
+
+adjustPartTwo :: Int -> Int
+adjustPartTwo x = 
     if x >= 3
       then x - 1
       else x + 1
 
-runDay5 :: OffsetAdjuster Int -> IO ()
+runDay5WithAdjuster :: (Read a, Step a) => (a -> a) -> IO ()
+runDay5WithAdjuster = runDay5 . OffsetAdjuster . fmap
+
+runDay5 :: (Read a, Step a) => OffsetAdjuster a -> IO ()
 runDay5 adjuster = do
   putStrLn "Test input... "
   printStepsToExit adjuster $ fmap Offset $ [0, 3, 0, 1, -3]
   putStrLn "Real input..."
   printStepsToExit adjuster <=<
-    (readInput :: FilePath -> IO [Offset Int]) <=< getDataFileName $
+    readInput <=< getDataFileName $
     "input/day5.txt"
 
 readInput :: (Read a) => FilePath -> IO [Offset a]
 readInput path = fmap (Offset . read) . lines <$> readFile path
 
-printStepsToExit :: (Step a, Show a) => OffsetAdjuster a -> [Offset a] -> IO ()
+printStepsToExit :: Step a => OffsetAdjuster a -> [Offset a] -> IO ()
 printStepsToExit adjuster = print . findStepsToExit adjuster
 
 newtype Offset a = Offset
@@ -41,7 +49,7 @@ newtype OffsetAdjuster a = OffsetAdjuster
   { _offsetAdjuster :: Offset a -> Offset a
   }
 
-type Step a = a ~ Int
+type Step a = (Eq a, Integral a, Show a, V.Unbox a)
 
 findStepsToExit ::
      forall a. Step a
@@ -58,8 +66,8 @@ findStepsToExit OffsetAdjuster {_offsetAdjuster = adjuster} =
       | trace ("index " ++ show index) False = undefined
       | index < 0 || index >= fromIntegral (V.length program) = pure 0
       | otherwise = do
-        nextIndex <- (index +) <$> V.read program index
-        V.modify program adjustment index
+        nextIndex <- (index +) <$> V.read program (fromIntegral index)
+        V.modify program adjustment (fromIntegral index)
         (+ 1) <$> go nextIndex program
       where
         adjustment = _offset . adjuster . Offset
