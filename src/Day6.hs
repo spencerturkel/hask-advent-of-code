73b8879388b76581f006a082041b9460653e3b56
Spencer Turkel <spencer@palmbeachsoftware.com>
2017-12-30 17:11:21 -0500

    Writing Day6 immutably

--- src/Day6.hs
+++ src/Day6.hs
@@ -1,12 +1,17 @@
 module Day6 where
 
--- import Control.Monad.ST (ST, stToIO)
 import Data.Function (on)
-
--- import Data.Monoid (Sum(Sum))
--- import Data.Set (Set)
--- import qualified Data.Set as Set
-import Data.Vector.Unboxed (Unbox, Vector, foldl', indexed, length, modify)
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.Vector.Unboxed
+  ( Unbox
+  , Vector
+  , foldl'
+  , fromList
+  , indexed
+  , length
+  , modify
+  )
 import qualified Data.Vector.Unboxed.Mutable as M (modify, write)
 import Prelude hiding (length)
 
@@ -15,6 +20,32 @@ testInput = [0, 2, 7, 0]
 
 actualInput :: [Int]
 actualInput = [2, 8, 8, 5, 4, 2, 3, 1, 5, 5, 1, 2, 15, 13, 5, 14]
+
+uniqueDistributionCycles ::
+     forall a. (Num a, Ord a, Unbox a)
+  => Vector a
+  -> Set (Vector a)
+uniqueDistributionCycles = foldEither acc Set.empty . iterate redistribute
+  where acc :: Vector a -> Set (Vector a) -> Either (Set (Vector a)) (Set (Vector a))
+        acc x xs = if Set.member x xs then Left xs else Right $ Set.union (Set.singleton x) xs
+
+foldEither :: (a -> b -> Either c b) -> b -> [a] -> c
+foldEither _ _ [] = undefined
+foldEither f y (x:xs) = case f x y of
+  Left z -> z
+  Right y' -> foldEither f y' xs
+
+redistributeTestInput :: Bool
+redistributeTestInput =
+  and $
+  fmap
+    (\(x, y) -> redistribute (fromList x) == fromList y)
+    [ ([0 :: Int, 2, 7, 0], [2, 4, 1, 2])
+    , ([2, 4, 1, 2], [3, 1, 2, 3])
+    , ([3, 1, 2, 3], [0, 2, 3, 4])
+    , ([0, 2, 3, 4], [1, 3, 4, 1])
+    , ([1, 3, 4, 1], [2, 4, 1, 2])
+    ]
 
 redistribute ::
      forall a. (Num a, Ord a, Unbox a)
