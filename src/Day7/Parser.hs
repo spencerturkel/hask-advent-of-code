e28c3226df1487d9b1a857c85bb4ad5a5d7ea31c
Spencer Turkel <spencer@palmbeachsoftware.com>
2017-12-31 00:06:28 -0500

    Finished Parser module

--- src/Day7/Parser.hs
+++ src/Day7/Parser.hs
@@ -1,47 +1,61 @@
-module Parser
-  ( parseInput
-  ) where
+module Parser where
 
-import Control.Applicative (Alternative(many, some))
-import Data.Set (Set, empty, fromList, member, singleton, union)
+import Control.Applicative (Alternative(some, (<|>)))
+import Control.Monad ((<=<))
+import Data.Char (isAlpha, isDigit)
+import Data.List (foldl')
+import Data.Set (Set, empty, fromList)
 import Text.ParserCombinators.ReadP
   ( ReadP
-  , ReadS
   , char
+  , option
   , readP_to_S
   , satisfy
+  , sepBy
   , skipMany
   , skipSpaces
+  , string
   )
 
 import ProgramInfo (ProgramInfo(ProgramInfo, _childProgramNames, _name))
 
-parseInput :: ReadS [ProgramInfo]
-parseInput = readP_to_S $ many pProgramInfo
+parseInput :: String -> Maybe ProgramInfo
+parseInput = toBestParse . allInputParses
+
+toBestParse :: forall a b. [(a, [b])] -> Maybe a
+toBestParse = fmap fst <$> foldl' acc Nothing
+  where
+    acc :: Maybe (a, Int) -> (a, [b]) -> Maybe (a, Int)
+    acc ((<|> Just (undefined, 0)) -> Just (x, xLen)) (y, length -> yLen) =
+      Just $ case compare xLen yLen of
+        LT -> (x, xLen)
+        _ -> (y, yLen)
+
+allInputParses :: String -> [(ProgramInfo, String)]
+allInputParses = (readP_to_S pProgramInfo) <=< lines
 
 pProgramInfo :: ReadP ProgramInfo
 pProgramInfo = do
-    skipSpaces
-    _name <- pName
-    skipSpaces
-    weight
-    skipSpaces
-    _childProgramNames <- pChildProgramNames
-    pure $ ProgramInfo {_name, _childProgramNames}
+  skipSpaces
+  _name <- pName
+  skipSpaces
+  weight
+  skipSpaces
+  _childProgramNames <-
+    option empty $ do
+      string "->"
+      skipSpaces
+      pChildProgramNames
+  pure $ ProgramInfo {_name, _childProgramNames}
 
 pName :: ReadP String
-pName = some (satisfy (/= ' '))
+pName = some (satisfy isAlpha)
 
 weight :: ReadP ()
-weight =
-  char '(' *> skipMany (satisfy . flip member . fromList $ "0123456789") <*
-   char ')'
+weight = char '(' *> skipMany (satisfy isDigit) <* char ')'
 
 pChildProgramNames :: ReadP (Set String)
-pChildProgramNames = go empty
-  where
-    go :: Set String -> ReadP (Set String)
-    go names = go . union names . singleton =<< pChildName
+pChildProgramNames = fromList <$> pChildName `sepBy` (char ',' *> skipSpaces)
 
 pChildName :: ReadP String
-pChildName = some (satisfy (/= ','))
+pChildName = some (satisfy isAlpha)
