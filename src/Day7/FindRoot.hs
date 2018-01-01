6e95b15b23be5b9d817ab0177c34ecdb78ffa4d6
Spencer Turkel <spencer@palmbeachsoftware.com>
2018-01-01 00:09:49 -0500

    Fix Day7 Part One

--- src/Day7/FindRoot.hs
+++ src/Day7/FindRoot.hs
@@ -1,24 +1,21 @@
 module FindRoot where
 
-import Control.Arrow (Arrow((&&&), first))
-import Data.Graph (graphFromEdges)
+import Control.Arrow (Arrow(first))
+import Data.Graph (graphFromEdges, topSort)
 import Data.List.NonEmpty (NonEmpty)
 import Data.List.NonEmpty as NonEmpty (toList)
-import Data.Set (fromList)
 import qualified Data.Set as Set (toList)
 
 import ProgramInfo (ProgramInfo(ProgramInfo, _childProgramNames, _name))
 
 findRootName :: NonEmpty ProgramInfo -> String
 findRootName =
-  fst .
-  fmap fromList .
-  (\(f, len) -> f (len - 1)) .
-  first
-    ((fmap (\((), z, a) -> (z, a)) .
-      (\(_, f, _) -> f) .
-      graphFromEdges .
-      fmap
-        (\(ProgramInfo {_name, _childProgramNames}) ->
-           ((), _name, Set.toList _childProgramNames)))) .
-  (id &&& length) . NonEmpty.toList
+  (\(_, x, _) -> x) .
+  (\((v, f)) -> f v) .
+  first (head . topSort) .
+  (\(g, f, _) -> (g, f)) .
+  graphFromEdges .
+  fmap
+    (\(ProgramInfo {_name, _childProgramNames}) ->
+       ((), _name, Set.toList _childProgramNames)) .
+  NonEmpty.toList
