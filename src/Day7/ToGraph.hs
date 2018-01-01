36bb3af39e7e886368962ecd07e556e0c64097cf
Spencer Turkel <spencer@palmbeachsoftware.com>
2018-01-01 00:28:29 -0500

    Add weight to graph

--- src/Day7/ToGraph.hs
+++ src/Day7/ToGraph.hs
@@ -5,14 +5,14 @@ import Data.List.NonEmpty (NonEmpty)
 import Data.List.NonEmpty as NonEmpty (toList)
 import qualified Data.Set as Set (toList)
 
-import ProgramInfo (ProgramInfo(ProgramInfo, _childProgramNames, _name))
+import ProgramInfo (ProgramInfo(ProgramInfo, _childProgramNames, _name, _weight))
 
 programInfoToGraph ::
      NonEmpty ProgramInfo
-  -> (Graph, Vertex -> ((), String, [String]), String -> Maybe Vertex)
+  -> (Graph, Vertex -> (Int, String, [String]), String -> Maybe Vertex)
 programInfoToGraph =
   graphFromEdges .
   fmap
-    (\(ProgramInfo {_name, _childProgramNames}) ->
-       ((), _name, Set.toList _childProgramNames)) .
+    (\(ProgramInfo {_name, _childProgramNames, _weight}) ->
+       (_weight, _name, Set.toList _childProgramNames)) .
   NonEmpty.toList
