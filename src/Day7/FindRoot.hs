032d98da5c6d026877698593fc3b212a453b4374
Spencer Turkel <spencer@palmbeachsoftware.com>
2018-01-01 00:20:38 -0500

    Extracted programInfoToGraph

--- src/Day7/FindRoot.hs
+++ src/Day7/FindRoot.hs
@@ -1,7 +1,7 @@
 module FindRoot where
 
 import Control.Arrow (Arrow(first))
-import Data.Graph (graphFromEdges, topSort)
+import Data.Graph (Graph, Vertex, graphFromEdges, topSort)
 import Data.List.NonEmpty (NonEmpty)
 import Data.List.NonEmpty as NonEmpty (toList)
 import qualified Data.Set as Set (toList)
@@ -12,8 +12,12 @@ findRootName :: NonEmpty ProgramInfo -> String
 findRootName =
   (\(_, x, _) -> x) .
   (\((v, f)) -> f v) .
-  first (head . topSort) .
-  (\(g, f, _) -> (g, f)) .
+  first (head . topSort) . (\(g, f, _) -> (g, f)) . programInfoToGraph
+
+programInfoToGraph ::
+     NonEmpty ProgramInfo
+  -> (Graph, Vertex -> ((), String, [String]), String -> Maybe Vertex)
+programInfoToGraph =
   graphFromEdges .
   fmap
     (\(ProgramInfo {_name, _childProgramNames}) ->
