a35953ad2df60507be7a5e145b33ff7c96f33b5e
Spencer Turkel <spencer@palmbeachsoftware.com>
2018-01-05 22:02:52 -0500

    Wrote Day8 Interpreter

---
+++ src/Day8/Interpreter.hs
@@ -0,0 +1,39 @@
+module Interpreter where
+
+import Data.Map.Lazy (Map, alter, findWithDefault)
+import Data.Maybe (fromMaybe)
+
+import Instruction
+  ( Comparison(Equal, GreaterThan, GreaterThanOrEqual, LessThan,
+           LessThanOrEqual, NotEqual)
+  , Instruction(Instruction, _comparator, _comparison, _operand,
+            _operation, _source, _target)
+  , Operation(Decrement, Increment)
+  )
+
+runInstruction :: Instruction -> Map String Int -> Map String Int
+runInstruction Instruction { _target
+                           , _operation
+                           , _operand
+                           , _source
+                           , _comparison
+                           , _comparator
+                           } state =
+  if runComparison _comparison (findWithDefault 0 _source state) _comparator
+    then alter
+           (Just . (flip (runOperation _operation) _operand) . fromMaybe 0)
+           _target
+           state
+    else state
+
+runOperation :: Num a => Operation -> a -> a -> a
+runOperation Increment = (+)
+runOperation Decrement = subtract
+
+runComparison :: Ord a => Comparison -> a -> a -> Bool
+runComparison LessThan = (<)
+runComparison LessThanOrEqual = (<=)
+runComparison NotEqual = (/=)
+runComparison Equal = (==)
+runComparison GreaterThan = (>)
+runComparison GreaterThanOrEqual = (>=)
